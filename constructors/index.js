b3e8b3619525e0cb33b8f34427b81b8fef709d71
Seia <30369714+Seia-Soto@users.noreply.github.com>
2019-05-06 07:01:17 +0900

    Re-structurize with request

--- constructors/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  Issue: require('./issue'),
-  Result: require('./result')
-}
--- index.js
+++ index.js
@@ -1,120 +1,110 @@
-var querystring = require('querystring');
+const querystring = require('querystring')
+const request = require('request-promise')
+const google_translate_token = require('google-translate-token')
 
-var got = require('got');
-var token = require('google-translate-token');
+const languages = require('./languages')
 
-var languages = require('./languages');
+const base_uri = 'https://translate.google.com/translate_a/single'
 
-function translate(text, opts) {
-    opts = opts || {};
+const translate = (text, options) => {
+  let error
+  options = options || {}
 
-    var e;
-    [opts.from, opts.to].forEach(function (lang) {
-        if (lang && !languages.isSupported(lang)) {
-            e = new Error();
-            e.code = 400;
-            e.message = 'The language \'' + lang + '\' is not supported';
-        }
-    });
-    if (e) {
-        return new Promise(function (resolve, reject) {
-            reject(e);
-        });
+  [options.from, options.to].forEach(language => {
+    if (language && !languages.isSupported(language)) {
+      error = new Error()
+      error.code = 400
+      error.message = `The language ${language} is not supported`
     }
+  })
+  if (error) return new Promise((resolve, reject) => reject(error))
 
-    opts.from = opts.from || 'auto';
-    opts.to = opts.to || 'en';
+  options.from = options.from || 'auto'
+  options.to = options.to || 'to'
 
-    opts.from = languages.getCode(opts.from);
-    opts.to = languages.getCode(opts.to);
+  options.from = languages.getCode(options.from)
+  options.to = languages.getCode(options.to)
 
-    return token.get(text).then(function (token) {
-        var url = 'https://translate.google.com/translate_a/single';
-        var data = {
-            client: 'gtx',
-            sl: opts.from,
-            tl: opts.to,
-            hl: opts.to,
-            dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
-            ie: 'UTF-8',
-            oe: 'UTF-8',
-            otf: 1,
-            ssel: 0,
-            tsel: 0,
-            kc: 7,
-            q: text
-        };
-        data[token.name] = token.value;
+  return google_translate_token.get(text).then(token => {
+    const params = querystring.stringify({
+      client: 'gtx',
+      sl: options.from,
+      tl: options.to,
+      hl: options.to,
+      dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
+      ie: 'UTF-8',
+      oe: 'UTF-8',
+      otf: 1,
+      ssel: 0,
+      tsel: 0,
+      kc: 7,
+      q: text
+    })
 
-        return url + '?' + querystring.stringify(data);
-    }).then(function (url) {
-        return got(url, {
-          headers: {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
+    return `${base_uri}?${params}`
+  }).then(url => {
+    return request({
+      url: url,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
+      }
+    })
+      .then(body => {
+        let result = {
+          text: '',
+          from: {
+            language: {
+              didYouMean: false,
+              iso: ''
+            },
+            text: {
+              autoCorrected: false,
+              value: '',
+              didYouMean: false
+            }
+          },
+          raw: ''
+        }
+
+        const data = JSON.parse(body)
+        if (options.raw) result.raw = body
+
+        data[0].forEach(objective => {
+          if (objective[0]) result.text += objective[0]
+        })
+
+        if (data[2] === data[8][0][0]) {
+          result.from.language.iso = data[2]
+        } else {
+          result.from.language.didYouMean = true
+          result.from.language.iso = data[8][0][0]
+        }
+
+        if (data[7] && data[7][0]) {
+          let string = data[7][0]
+
+          string = string.replace(/<b><i>/g, '[')
+          string = string.replace(/<\/i><\/b>/g, ']')
+
+          result.from.text.value = string
+
+          if (data[7][5] === true) {
+            result.from.text.autoCorrected = true
+          } else {
+            result.from.text.didYouMean = true
           }
-        }).then(function (res) {
-            var result = {
-                text: '',
-                from: {
-                    language: {
-                        didYouMean: false,
-                        iso: ''
-                    },
-                    text: {
-                        autoCorrected: false,
-                        value: '',
-                        didYouMean: false
-                    }
-                },
-                raw: ''
-            };
+        }
 
-            if (opts.raw) {
-                result.raw = res.body;
-            }
+        return result
+      })
+      .catch(error => {
+        error = new Error()
+        error.code = 'BAD_REQUEST'
 
-            var body = JSON.parse(res.body);
-            body[0].forEach(function (obj) {
-                if (obj[0]) {
-                    result.text += obj[0];
-                }
-            });
-
-            if (body[2] === body[8][0][0]) {
-                result.from.language.iso = body[2];
-            } else {
-                result.from.language.didYouMean = true;
-                result.from.language.iso = body[8][0][0];
-            }
-
-            if (body[7] && body[7][0]) {
-                var str = body[7][0];
-
-                str = str.replace(/<b><i>/g, '[');
-                str = str.replace(/<\/i><\/b>/g, ']');
-
-                result.from.text.value = str;
-
-                if (body[7][5] === true) {
-                    result.from.text.autoCorrected = true;
-                } else {
-                    result.from.text.didYouMean = true;
-                }
-            }
-
-            return result;
-        }).catch(function (err) {
-            var e;
-            e = new Error();
-            if (err.statusCode !== undefined && err.statusCode !== 200) {
-                e.code = 'BAD_REQUEST';
-            } else {
-                e.code = 'BAD_NETWORK';
-            }
-            throw e;
-        });
-    });
+        throw error
+      })
+  })
 }
 
-module.exports = translate;
-module.exports.languages = languages;
+module.exports = translate
+module.exports.languages = languages
